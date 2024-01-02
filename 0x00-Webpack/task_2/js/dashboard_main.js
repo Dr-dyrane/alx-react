c5f8f4fe77c85d072e4998532f842b5541e7f04f
Dr-dyrane <halodyrane@gmail.com>
2024-01-02 15:27:04 +0100

    FIX

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,23 +1,34 @@
-const $ = require( "jquery" );
-// Load the full build
-const _ = require("lodash");
-import "../css/main.css";
+const $ = require('jquery');
+const _ = require('lodash');
+import '../css/main.css';
 
 let count = 0;
 
 function updateCounter() {
   count++;
-  $( "#count" ).text(`${count} clicks on the button` );
+  $('#count').text(`${count} clicks on the button`);
 }
 
-const $button = $("<button>Click here to get started</button>").on(
-  "click",
-  _.debounce( updateCounter, 500, { leading: true, trailing: false })
-);
+function createButton() {
+  const $button = $('<button>Click here to get started</button>').on(
+    'click',
+    _.debounce(updateCounter, 500, { leading: true, trailing: false })
+  );
+  return $button;
+}
 
-$('body').append("<div id='logo'></div>");
-$('body').append("<p>Holberton Dashboard</p>");
-$('body').append("<p>Dashboard data for the students</p>");
-$('body').append($button);
-$('body').append("<p id='count'></p>");
-$('body').append("<p>Copyright - Holberton School</p>");
+function createLogo() {
+  return $('<div id="logo"></div>');
+}
+
+function appendElements() {
+  $('body').append(createLogo());
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append(createButton());
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+}
+
+// Call the function to append elements when the script runs
+appendElements();
