b1370c30ce003b257ea2bac954ec70669952c48e
Dr-dyrane <halodyrane@gmail.com>
2024-01-03 15:48:02 +0100

    task 5

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,32 @@
+// task_5/dashboard/config/webpack.config.js
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
---
+++ 0x01-react_intro/task_5/dashboard/src/App/index.js
@@ -0,0 +1,12 @@
+// task_5/dashboard/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
