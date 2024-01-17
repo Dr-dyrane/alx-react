3e73829ab94083b9cc6e751edd3cc06f532aa751
Dr-dyrane <halodyrane@gmail.com>
2024-01-17 20:57:27 +0100

    modified:   0x04-React_inline_styling/task_1/dashboard/src/Footer/Footer.js

--- 0x04-React_inline_styling/task_1/dashboard/src/Footer/Footer.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Footer/Footer.js
@@ -1,15 +1,25 @@
 import React from "react";
-import "./Footer.css";
+import { StyleSheet, css } from "aphrodite";
 import { getFullYear, getFooterCopy } from "../utils/utils";
 
 function Footer() {
   return (
     <>
-      <div className="App-footer">
+      <div className={css(styles.footer)}>
         Copyright {getFullYear()} - {getFooterCopy()}
       </div>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  footer: {
+    fontSize: '1rem',
+    padding: '1.2rem',
+    textAlign: 'center',
+    fontStyle: 'italic',
+    borderTop: '3px solid #e0354b',
+  },
+});
 
 export default Footer;
