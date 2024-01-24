dad0ce93f45d7db6a50a3ff4d38c0313df796ce9
Dr-dyrane <halodyrane@gmail.com>
2024-01-24 17:49:35 +0100

    modified:   0x06-React_Immutable/8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,5 +1,5 @@
 // Import the necessary module from Immutable.js library
-import { Seq } from 'immutable';
+import { Seq } from "immutable";
 
 /**
  * Filters and prints best students with a score >= 70.
@@ -7,23 +7,23 @@ import { Seq } from 'immutable';
  * @param {Object} object - An object containing student information.
  */
 export default function printBestStudents(object) {
-  // Create a lazy sequence from the input object using Seq
-  const seq = Seq(object);
+	// Create a lazy sequence from the input object using Seq
+	const seq = Seq(object);
 
-  // Filter students with a score > 70 and transform their names
-  const filtered = seq
-    .filter((student) => student.score > 70)
-    .map((student) => ({
-      ...student,
-      firstName: capFirstLetter(student.firstName),
-      lastName: capFirstLetter(student.lastName),
-    }));
+	// Filter students with a score > 70 and transform their names
+	const filtered = seq
+		.filter((student) => student.score > 70)
+		.map((student) => ({
+			...student,
+			firstName: capFirstLetter(student.firstName),
+			lastName: capFirstLetter(student.lastName),
+		}));
 
-  // Convert the lazy sequence to a JavaScript object
-  const JSObject = filtered.toJS();
+	// Convert the lazy sequence to a JavaScript object
+	const JSObject = filtered.toJS();
 
-  // Print the resulting object to the console
-  console.log(JSObject);
+	// Print the resulting object to the console
+	console.log(JSObject);
 }
 
 /**
@@ -33,5 +33,5 @@ export default function printBestStudents(object) {
  * @returns {string} - The string with the first letter capitalized.
  */
 function capFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
