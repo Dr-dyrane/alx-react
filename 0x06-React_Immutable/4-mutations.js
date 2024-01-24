0432a394703c0e115ce12f107e183bd5548b1be6
Dr-dyrane <halodyrane@gmail.com>
2024-01-24 17:25:29 +0100

    Merge & concat

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,5 +1,7 @@
+// Import the necessary module from Immutable.js library
 import { Map } from 'immutable';
 
+// Create an Immutable Map named 'map'
 export const map = Map({
   1: 'Liam',
   2: 'Noah',
@@ -9,6 +11,9 @@ export const map = Map({
   6: 'Lucas'
 });
 
+// Export a second constant named 'map2'
 export const map2 = map.withMutations((values) => {
-  values.set(2, 'Benjamin').set(4, 'Oliver');
+  values
+  .set(2, 'Benjamin') // Modify the value for index 2 to 'Benjamin'
+  .set(4, 'Oliver'); // Modify the value for index 4 to 'Oliver'
 });
--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,5 +1,36 @@
+// Import the necessary modules from Immutable.js library
 import { List, Map } from 'immutable';
 
-export const concatElements = (page1, page2) => List(page1).concat(List(page2));
+/**
+ * Concatenates two arrays into an Immutable List.
+ *
+ * @param {Array} page1 - The first array.
+ * @param {Array} page2 - The second array.
+ * @returns {List} - Immutable List containing the values of both arrays.
+ */
+export function concatElements(page1, page2) {
+  // Use List to create an Immutable List by concatenating the two arrays
+  const concatenatedList = List(page1).concat(List(page2));
 
-export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
+  // Return the resulting Immutable List
+  return concatenatedList;
+}
+
+/**
+ * Merges two objects into an Immutable List.
+ *
+ * @param {Object} page1 - The first object.
+ * @param {Object} page2 - The second object.
+ * @returns {List} - Immutable List containing the values of both objects.
+ */
+export function mergeElements(page1, page2) {
+  // Use Map to create Immutable Maps from the input objects
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+
+  // Use mergeWith to merge the two Maps, giving preference to values from page2
+  const mergedMap = map1.mergeWith((oldVal, newVal) => newVal, map2);
+
+  // Return the values of the merged Map as an Immutable List
+  return mergedMap.toList();
+}
